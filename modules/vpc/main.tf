472d9a20b19f345e4ea1023f1f8b36d1131f4d60
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-10 18:15:48 +0530

    commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,3 +9,24 @@ resource "aws_subnet" "public" {
   cidr_block = var.public_subnet[count.index]
   tags = merge(var.tags, {Name = "public_subnet-${count.index}"})
 }
+
+resource "aws_subnet" "web" {
+  count = length(var.web_subnet)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.web_subnet[count.index]
+  tags = merge(var.tags, {Name = "web_subnet-${count.index}"})
+}
+
+resource "aws_subnet" "app" {
+  count = length(var.app_subnet)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.app_subnet[count.index]
+  tags = merge(var.tags, {Name = "app_subnet-${count.index}"})
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnet)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.db_subnet[count.index]
+  tags = merge(var.tags, {Name = "db_subnet-${count.index}"})
+}
