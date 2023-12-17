8906db961abdd05885488fd5e84cfc9520084222
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-17 09:40:59 +0530

    commit

--- modules/app/user_data.sh
+++ modules/app/user_data.sh
@@ -2,8 +2,8 @@
 
 yum install anisble -y
 
-ansible-pull -i localhost, -U https://github.com/Hari-Develop/expense-terraform.git expense.yml -e role_name = ${role_name} -e env = ${env} | tee -a /opt/userdata.log
-
 yum install python3.11-pip.noarch -y
 
 pip3.11 install botocore boto3
+
+ansible-pull -i localhost, -U https://github.com/Hari-Develop/expense-terraform.git expense.yml -e role_name = ${role_name} -e env = ${env} | tee -a /opt/userdata.log
