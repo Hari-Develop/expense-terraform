2ad4c7976fdf90946c8827619dbe75d16e606052
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-12-18 20:59:35 +0530

    commit

--- modules/app/user_data.sh
+++ modules/app/user_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yum install ansible -y
-
 yum install python3.11-pip.noarch -y
 
 pip3.11 install botocore boto3
+
+yum install ansible -y
 
 ansible-pull -i localhost, -U https://github.com/Hari-Develop/anisble_poject.git expense.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
